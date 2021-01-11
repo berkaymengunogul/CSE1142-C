574dfa6fab86aa4b10a5497f1b170f5a5ac449ed
berkaymengunogul <berkay.mengunogul@outlook.com>
2021-01-11 12:42:42 +0300

    Structure made

--- HW3/150119934HW3.c
+++ HW3/150119934HW3.c
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct {
     int len;
@@ -20,15 +21,37 @@ unsigned int findScore(String *s1){
     return 0;
 }
 int main(void){
-    char input[50];
-    char exit[] = "exit";
-    char quit[] = "quit"
-    scanf("%s", input);
-    while ((strcmp(input,"exit") != 0||strcmp(input,"quit") != 0)){
-        if (strcmp(input, "stat") == 0){
-            printf("here");
+    FILE *fPtr;
+    if ((fPtr = fopen("input.txt", "r")) !=NULL){
+        char input[50];
+        fscanf(fPtr, "%49[^\n]", input);
+        sscanf("%s", input);
+        fclose(fPtr);
+        return 0;
+        int numOfWords = 0, index;
+        long int lenInput = 0;
+        while (!((strcmp(input,"exit") == 0 )|| (strcmp(input,"quit") == 0))){
+            if (strcmp(input, "stat") == 0){
+                printf("The number of words: %d\n", numOfWords);
+                printf("The number of alphabetic letters: %lu\n", lenInput);
+            }else{
+                numOfWords = 0;
+                lenInput = 0;
+                char *token = strtok(input, " ");
+                while(token != NULL){
+                    for(index = 0; index<strlen(token); index++){
+                        if(isalpha(token[index])){
+                            lenInput++;
+                        }
+                    }
+                    numOfWords++;
+                    token = strtok(NULL, " ");
+                }
+            }
+            scanf(" %49[^\n]", input);
         }
-        scanf("%s", input);
+    }else{
+        printf("error");
     }
     return 0;
 }
