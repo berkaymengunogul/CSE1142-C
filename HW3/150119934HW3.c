043da643051ead588428def17943d396c56ab6c7
skynet <berkay.mengunogul@outlook.com>
2021-01-12 16:42:50 +0300

    Command line arguments added

--- HW3/150119934HW3.c
+++ HW3/150119934HW3.c
@@ -111,74 +111,84 @@ unsigned int findScore(String *s1){
 
     return score;
 }
-int main(void){
+int main(int argc, char *argv[]){
     FILE *inputFilePtr;
+    FILE *outputFilePtr;
     // file opened here, if succeed operations will continue
-    if ((inputFilePtr = fopen("input.txt", "r")) !=NULL){
-        char input[100];
-        char *tokenSentence, *tokenInstructions;
-        int numOfWords = 0, index;
-        long int lenInput = 0;
+    if ((inputFilePtr = fopen(argv[1], "r")) !=NULL){
+        if((outputFilePtr = fopen(argv[2], "w")) !=NULL){
+            char input[100];
+            char *tokenSentence, *tokenInstructions;
+            int numOfWords = 0, index;
+            long int lenInput = 0;
 
-        while (!feof(inputFilePtr)) {
-            fscanf(inputFilePtr, "%[^\n]\n", input);
+            while (!feof(inputFilePtr)) {
+                fscanf(inputFilePtr, "%[^\n]\n", input);
 
 
-            tokenSentence = strtok(input, ":"); // Sentence is read till ":"
-            tokenInstructions = strtok(NULL, ","); // Part after the ":" stored here
+                tokenSentence = strtok(input, ":"); // Sentence is read till ":"
+                tokenInstructions = strtok(NULL, ","); // Part after the ":" stored here
 
-            // String node created here and values assigned to it.
-            String *node = malloc(sizeof(String));
-            strcpy((*node).data, tokenSentence);
-            (*node).len = strlen(tokenSentence);
+                // String node created here and values assigned to it.
+                String *node = malloc(sizeof(String));
+                strcpy((*node).data, tokenSentence);
+                (*node).len = strlen(tokenSentence);
 
-            String *string1 = malloc(sizeof(String));
+                String *string1 = malloc(sizeof(String));
 
-            if (strcmp(input, "stat") == 0) {
-                printf("The number of words: %d\n", numOfWords);
-                printf("The number of alphabetic letters: %lu\n", lenInput);
-            }else if((strcmp(input, "exit") == 0) || (strcmp(input, "quit") == 0)){
-                printf("Program ends. Bye\n");
-            }else if (strcmp(tokenInstructions, "1") == 0){
-                int result = charAt(node, atoi(strtok(NULL, ",")));
-                printf("%c\n", result);
-            }else if(strcmp(tokenInstructions, "2") == 0){
-                strcpy((*string1).data, strtok(NULL, ","));
-                (*string1).len = strlen(string1->data);
-                printf("%s\n", (*concat(node, string1)).data);
-                tokenInstructions = strtok(NULL, ",");
-            }else if(strcmp(tokenInstructions, "3") == 0){
-                strcpy((*string1).data, strtok(NULL, ","));
-                (*string1).len = strlen(string1->data);
-                printf("%d\n", strSearch(node, string1));
-                tokenInstructions = strtok(NULL, ",");
-            }else if(strcmp(tokenInstructions, "4") == 0){
-                printf("%d\n", findScore(node));
-            }
-            char *tokenState = strtok(tokenSentence, " ");
-            while(tokenState != NULL){
-                for(index = 0; index<strlen(tokenState); index++){
-                    if(isalpha(tokenState[index])){
-                        lenInput++;
+                if (strcmp(input, "stat") == 0) {
+                    fprintf(outputFilePtr,"The number of words: %d\n", numOfWords);
+                    fprintf(outputFilePtr,"The number of alphabetic letters: %lu\n", lenInput);
+                }else if((strcmp(input, "exit") == 0) || (strcmp(input, "quit") == 0)){
+                    fprintf(outputFilePtr,"Program ends. Bye\n");
+                }else if (strcmp(tokenInstructions, "1") == 0){
+                    int result = charAt(node, atoi(strtok(NULL, ",")));
+                    if(result == -1){
+                        fprintf(outputFilePtr, "%d\n", result);
+                    }else {
+                        fprintf(outputFilePtr, "%c\n", result);
                     }
+                }else if(strcmp(tokenInstructions, "2") == 0){
+                    strcpy((*string1).data, strtok(NULL, ","));
+                    (*string1).len = strlen(string1->data);
+                    fprintf(outputFilePtr,"%s\n", (*concat(node, string1)).data);
+                    tokenInstructions = strtok(NULL, ",");
+                }else if(strcmp(tokenInstructions, "3") == 0){
+                    strcpy((*string1).data, strtok(NULL, ","));
+                    (*string1).len = strlen(string1->data);
+                    fprintf(outputFilePtr,"%d\n", strSearch(node, string1));
+                    tokenInstructions = strtok(NULL, ",");
+                }else if(strcmp(tokenInstructions, "4") == 0){
+                    fprintf(outputFilePtr,"%d\n", findScore(node));
                 }
-                numOfWords++;
-                tokenState = strtok(NULL, " ");
-            }
-            tokenState = strtok(string1->data, " ");
-            while(tokenState != NULL){
-                for(index = 0; index<strlen(tokenState); index++){
-                    if(isalpha(tokenState[index])){
-                        lenInput++;
+                char *tokenState = strtok(tokenSentence, " ");
+                while(tokenState != NULL){
+                    for(index = 0; index<strlen(tokenState); index++){
+                        if(isalpha(tokenState[index])){
+                            lenInput++;
+                        }
                     }
+                    numOfWords++;
+                    tokenState = strtok(NULL, " ");
                 }
-                numOfWords++;
-                tokenState = strtok(NULL, ",");
+                tokenState = strtok(string1->data, " ");
+                while(tokenState != NULL){
+                    for(index = 0; index<strlen(tokenState); index++){
+                        if(isalpha(tokenState[index])){
+                            lenInput++;
+                        }
+                    }
+                    numOfWords++;
+                    tokenState = strtok(NULL, ",");
+                }
+                free(node);
+                free(string1);
             }
-            free(node);
-            free(string1);
+            fclose(inputFilePtr);
+            fclose(outputFilePtr);
+        }else{
+            printf("error");
         }
-        fclose(inputFilePtr);
     }else{
         printf("error");
     }
